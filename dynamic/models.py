88ef5b7da4d6ba40aedf5006c8e139ee550fb5d6
caoxunaaa <451501060@qq.com>
2020-07-07 15:49:45 +0800

    树洞项目启动

---
+++ dynamic/models.py
@@ -0,0 +1,24 @@
+from django.db import models
+from django.contrib.auth.models import User
+
+
+class DynamicType(models.Model):
+    type_name = models.CharField(verbose_name='类型名称', max_length=10)
+
+    def __str__(self):
+        return self.type_name
+
+
+class Dynamic(models.Model):
+    owner = models.ForeignKey(User, verbose_name='发起人', on_delete=models.CASCADE)
+    text = models.TextField(verbose_name='正文')
+    type = models.ForeignKey(DynamicType, verbose_name='标签', on_delete=models.CASCADE)
+
+    created_time = models.DateTimeField(verbose_name='创建时间', auto_now_add=True)
+    update_time = models.DateTimeField(verbose_name='更新时间', auto_now=True)
+
+    def __str__(self):
+        return self.text[:10]
+
+    class Meta:
+        ordering = ['-update_time']
