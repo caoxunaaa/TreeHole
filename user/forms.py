f12ada476c1a6a6aff6db14f32a45286d633af90
caoxunaaa <451501060@qq.com>
2020-07-14 11:28:00 +0800

    使用CreateView进行用户注册

--- user/forms.py
+++ user/forms.py
@@ -1,12 +1,46 @@
 from django.contrib.auth import forms as fm
 from django import forms
+from django.contrib.auth.models import User
 
 
 class LoginForm(fm.AuthenticationForm):
-    username = fm.UsernameField(label='用户名', widget=forms.TextInput(attrs={'class': 'form-control', 'placeholder': '请输入3-30位用户名'}))
+    username = fm.UsernameField(label='用户名',
+                                widget=forms.TextInput(attrs={'class': 'form-control', 'placeholder': '请输入3-20位用户名'}))
     password = forms.CharField(
         label='密码',
         strip=False,
         widget=forms.PasswordInput(attrs={'class': 'form-control', 'placeholder': '请输入密码'}),
     )
 
+
+class RegisterForm(forms.ModelForm):
+    username = forms.CharField(label='用户名',
+                               widget=forms.TextInput(attrs={'class': 'form-control', 'placeholder': '请输入3-20位用户名'}))
+    email = forms.EmailField(label='邮箱',
+                             required=False,
+                             widget=forms.EmailInput(attrs={'class': 'form-control', 'placeholder': '请输入邮箱,可以为空'}))
+    password = forms.CharField(
+        label='密码',
+        widget=forms.PasswordInput(attrs={'class': 'form-control', 'placeholder': '请输入密码'}),
+    )
+    password_again = forms.CharField(
+        label='重复密码',
+        widget=forms.PasswordInput(attrs={'class': 'form-control', 'placeholder': '请输入密码'}),
+    )
+
+    class Meta:
+        model = User
+        fields = ['username', 'email', 'password', 'password_again']
+
+    def clean_password_again(self):
+        password = self.cleaned_data['password']
+        password_again = self.cleaned_data['password_again']
+        if password != password_again:
+            raise forms.ValidationError('两次密码不一致')
+        return password_again
+
+    def clean_username(self):
+        username = self.cleaned_data['username']
+        if User.objects.filter(username=username).exists():
+            raise forms.ValidationError('用户名已经存在')
+        return username
--- user/views.py
+++ user/views.py
@@ -1,5 +1,10 @@
 from django.contrib.auth.views import LoginView, LogoutView
-from .forms import LoginForm
+from django.views.generic import CreateView
+from .forms import LoginForm, RegisterForm
+from django.contrib.auth.models import User
+from django.contrib import auth
+from django.shortcuts import redirect
+from django.urls import reverse
 
 
 class Login(LoginView):
@@ -9,5 +14,23 @@ class Login(LoginView):
 
 class Logout(LogoutView):
     pass
+
+
+class Register(CreateView):
+    form_class = RegisterForm
+    template_name = 'user/register.html'
+    success_url = 'login'
+
+    def form_valid(self, form):
+        username = form.cleaned_data['username']
+        email = form.cleaned_data.get('email', '')
+        password = form.cleaned_data['password_again']
+        user = User.objects.create_user(username=username, email=email, password=password)
+        user.save()
+
+        user = auth.authenticate(username=username, password=password)
+        auth.login(self.request, user)
+        return redirect(self.request.GET.get('next', reverse('home')))
+
 
 
